e5aa57e726f3fc1296ecd96a310bad751788e98f
José Matoso <jos3matosoj@gmail.com>
2022-02-19 00:32:06 +0100

    v2

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,12 +1,39 @@
 
 window.addEventListener('load', () => {
     const loader = $('#loader')
+
     setTimeout(() => {
         loader.addClass('fadeOut')
     }, 300)
 
     feather.replace()
+
     $('#copy').text(new Date().getFullYear())
+
+    $(".owl-carousel").owlCarousel(
+        {
+            loop:true,
+            margin:10,
+            autoplay:true,
+            nav:true,
+            lazyLoad:true,
+            items: 3,
+            responsive:{
+                0:{
+                    items:1,
+                    nav:true
+                },
+                600:{
+                    items:3,
+                    nav:true
+                },
+                1000:{
+                    items:5,
+                    nav:true
+                }
+            }
+        }
+    );
 })
 
 $(window).bind('scroll', () => {
